a8ad46ad089e6cc832b6a44d31d5f23619c39416
RZ-Music <ryanzhu2005@gmail.com>
2022-12-23 17:54:55 -0500

    api call

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  link: string;
+  response: any;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  fetch(req.body.link).then((response) => {
+    response.text().then((data) => {
+      res.status(200).json({
+        link: req.body.link,
+        response: data,
+      });
+    });
+  });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,33 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useState } from "react";
 
 export default function Home() {
+  const [link, setLink] = useState("");
+
+  const getStats = () => {
+    console.log("Getting stats for " + link);
+
+    fetch("/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        link: link,
+      }),
+    }).then((res) => {
+      res.json().then((data) => {
+        let response = data.response.split('data-content="')[1];
+        response = response.split("></div>")[0];
+        response = response.trim();
+        response = response.slice(0, -1);
+        response = response.replaceAll("&quot;", '"');
+        response = JSON.parse(response);
+        console.log(response);
+      });
+    });
+  };
+
   return (
     <>
       <Head>
@@ -16,7 +39,33 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="w-screen h-screen"></main>
+      <main className="w-screen h-screen flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold">MuseStats</h1>
+        <h2 className="text-2xl font-medium mt-4">
+          Get statistics for any MuseScore account!
+        </h2>
+        <div>
+          <p className="text-xl font-medium mt-8">
+            Enter a valid MuseScore profile link.
+          </p>
+          <input
+            type="text"
+            placeholder="e.g. https://musescore.com/user/34214067"
+            className="w-96 h-12 mt-4 rounded-lg border-2 border-gray-300 focus:outline-none focus:border-blue-500 placeholder:italic"
+            onChange={(e) => setLink(e.target.value)}
+          />
+          <button
+            className="w-96 h-12 mt-4 rounded-lg bg-blue-500 text-white font-medium"
+            onClick={() => {
+              if (link) {
+                getStats();
+              }
+            }}
+          >
+            Get Stats
+          </button>
+        </div>
+      </main>
     </>
   );
 }
